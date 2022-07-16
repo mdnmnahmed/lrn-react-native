fa0f7dbf86b856de9e36112df3cb58832448a507
Numan Ahmed <mdnmnahmed@gmail.com>
2022-07-16 23:48:26 +0530

    Home Component UI Design, Added Switch Component with switching Functionality & added List View component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, ImageBackground, Text, View, ScrollView, TextInput, Touchable, TouchableOpacity, StyleSheet } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import { SimpleCarousel, Banner } from 'react-native-simple-banner-carousel';
 import { sliderData } from '../modals/data';
+import CustomSwitch from '../components/SharedComponents/CustomSwitch';
+import ListView from '../components/SharedComponents/ListView';
 
-const Home = () => {
+const switchButtonsData = ['free', 'premium']
+
+const Home = ({ navigation }) => {
+    const [selectedSwitch, setSelectedSwitch] = useState(switchButtonsData[0]);
+
 
     return (
         <>
@@ -14,11 +20,13 @@ const Home = () => {
                         <Text style={{ fontSize: 16, fontWeight: 'bold', color: 'black' }}>
                             Hello, Numan
                         </Text>
-                        <ImageBackground
-                            source={require('../assets/images/users/avatar-9.jpg')}
-                            style={{ width: 35, height: 35 }}
-                            imageStyle={{ borderRadius: 25 }}
-                        />
+                        <TouchableOpacity onPress={() => navigation.goBack()}>
+                            <ImageBackground
+                                source={require('../assets/images/users/avatar-9.jpg')}
+                                style={{ width: 35, height: 35 }}
+                                imageStyle={{ borderRadius: 25 }}
+                            />
+                        </TouchableOpacity>
                     </View>
 
                     <View style={{ flexDirection: 'row', borderColor: '#C6C6C6', borderWidth: 1, borderRadius: 8, paddingHorizontal: 10, marginTop: 10 }}>
@@ -49,6 +57,27 @@ const Home = () => {
                                 )
                             }}
                         />
+                    </View>
+
+                    <View style={{ paddingTop: 20 }}>
+                        <CustomSwitch
+                            selectedSwitch={selectedSwitch}
+                            switchButtons={switchButtonsData}
+                            setSelectedSwitch={setSelectedSwitch}
+                        />
+
+                        <View style={{ marginVertical: 15, marginBottom: 40 }}>
+                            {sliderData.map((data, index) => (
+                                <View key={index}>
+                                    {selectedSwitch == 'premium' && data.price != '0' && (
+                                        <ListView img={data.img} title={data.title} tag={data.tag} btnTxt={`Pay ${data.price}`} />
+                                    )}
+                                    {selectedSwitch == 'free' && data.price == '0' && (
+                                        <ListView img={data.img} title={data.title} tag={data.tag} btnTxt={`View`} />
+                                    )}
+                                </View>
+                            ))}
+                        </View>
                     </View>
 
                 </ScrollView>
