d04e5a8e3964110573cd26be50099ea08cb7da77
Numan Ahmed <mdnmnahmed@gmail.com>
2022-07-16 08:13:58 +0530

    Previous code cleanups

--- App.js
+++ App.js
@@ -1,50 +1,9 @@
 import React from 'react';
-
 import { useState } from 'react';
-
-
 import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View, FlatList, TouchableWithoutFeedback, Keyboard, TouchableOpacity } from 'react-native';
-
-import {
-    Colors,
-    // DebugInstructions,
-    // // Header,
-    // LearnMoreLinks,
-    // ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import CharacterLappy from './assets/images/character_lappy.svg';
-
-// import Header from './src/components/Header';
-// import TodoItem from './src/components/TodoItem';
-// import AddTodo from './src/components/AddTodo';
-
-// const Section = ({ children, title }) => {
-//     const isDarkMode = useColorScheme() === 'dark';
-//     return (
-//         <View style={styles.sectionContainer}>
-//             <Text
-//                 style={[
-//                     styles.sectionTitle,
-//                     {
-//                         color: isDarkMode ? Colors.white : Colors.black,
-//                     },
-//                 ]}>
-//                 {title}
-//             </Text>
-//             <Text
-//                 style={[
-//                     styles.sectionDescription,
-//                     {
-//                         color: isDarkMode ? Colors.light : Colors.dark,
-//                     },
-//                 ]}>
-//                 {children}
-//             </Text>
-//         </View>
-//     );
-// };
 
 const App = () => {
     const isDarkMode = useColorScheme() === 'dark';
@@ -52,28 +11,6 @@ const App = () => {
     const backgroundStyle = {
         backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     };
-
-    // const [todos, setTodos] = useState([
-    //     { text: 'write some code', id: 1 },
-    //     { text: 'create an App', id: 2 },
-    //     { text: 'play some game', id: 3 }
-    // ]);
-
-    // const deleteTodo = (itemId) => {
-    //     setTodos((prevTodos) => {
-    //         return prevTodos.filter(todo => todo.id !== itemId);
-    //     });
-    // }
-
-    // const addTodo = (newTodo) => {
-    //     setTodos((prevTodos) => {
-    //         return [newTodo, ...prevTodos]
-    //     })
-    // }
-
-    // const dismissKeyboard = () => {
-    //     Keyboard.dismiss();
-    // }
 
     return (
         <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
@@ -96,31 +33,5 @@ const App = () => {
         </SafeAreaView>
     );
 };
-
-// const styles = StyleSheet.create({
-//     sectionContainer: {
-//         marginTop: 32,
-//         paddingHorizontal: 24,
-//     },
-//     sectionTitle: {
-//         fontSize: 24,
-//         fontWeight: '600',
-//     },
-//     sectionDescription: {
-//         marginTop: 8,
-//         fontSize: 18,
-//         fontWeight: '400',
-//     },
-//     highlight: {
-//         fontWeight: '700',
-//     },
-//     content: {
-//         padding: 40,
-
-//     },
-//     list: {
-//         marginTop: 20
-//     }
-// });
 
 export default App;
